d69facd34da2b076a06f6e3e900160fdb7c980c4
C.J. May <doctormay6@gmail.com>
2022-07-27 15:34:46 -0500

    shrunk to just agent code

--- Commander/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv bash
-
-# start flask service
-gunicorn -b 0.0.0.0:5000 \
-         --workers $WORKERS \
-		 --worker-connections $WORKER_CONNECTIONS \
-		 --worker-class gevent \
-		 --worker-tmp-dir /dev/shm \
-		 server:app
